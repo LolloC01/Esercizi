f4576e7c0dfae93561ab2e98c38eb9b84186c0eb
LolloC01 <colittolorenzo@gmail.com>
2024-04-22 13:09:11 +0200

    8.6

--- Lezione4/main.py
+++ Lezione4/main.py
@@ -20,3 +20,19 @@ while es != 0:
         taglia: str = "XXL"
         testo: str = "DAJE ROMA DAJE"
         lezione4.make_shirt_1(taglia, testo)
+        lezione4.make_shirt_1()
+        lezione4.make_shirt_1("M")
+    elif es == 8.5:
+        lezione4.describe_city("Roma")
+        lezione4.describe_city("Milano")
+        lezione4.describe_city("Miami", "USA")
+    elif es == 8.6:
+        citta: str = "Roma"
+        paese: str = "Italia"
+        print(lezione4.city_country(citta,paese))
+        citta: str = "Parigi"
+        paese: str = "Francia"
+        print(lezione4.city_country(citta,paese))
+        citta: str = "Londra"
+        paese: str = "Regno Unito"
+        print(lezione4.city_country(citta,paese))

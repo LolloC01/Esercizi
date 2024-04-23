38d9e374c7d37372bcbac2ecbdd2626660663791
LolloC01 <colittolorenzo@gmail.com>
2024-04-23 12:09:04 +0200

    esercizi 1 e 2

---
+++ Lezione4/main_2.py
@@ -0,0 +1,22 @@
+import lezione4_2 as es
+
+
+'''students: list = []
+while True:
+    s: str = input("Inserisci il nome dello studente (premi 0 per terminare): ")
+    if s == "0":
+        break
+    else:
+        students.append(s)
+for s in students:
+    votes: list = []
+    while True:
+        x: int = int(input(f"Inserisci i voti dello studente {s}(premi 0 per terminare): "))
+        if x == 0:
+            break
+        else:
+            votes.append(x)
+    es.average_score(s,votes)'''
+
+
+'''es.game(es.rand(1,10),10)'''

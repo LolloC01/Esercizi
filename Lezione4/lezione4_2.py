e6a4a3639155784d03e6940240ac4249aa0fd33a
LolloC01 <colittolorenzo@gmail.com>
2024-04-24 09:26:02 +0200

    ..

--- Lezione4/lezione4_2.py
+++ Lezione4/lezione4_2.py
@@ -104,10 +104,15 @@ def cart(carrello: list, oper: int, discount: float = 0, taxes: float = 22) -> l
      Create a function that reads a text file and counts the number of occurrences of each word.
     The function should print a report showing the most frequent words and their number of occurrences.
     You can use a for loop to iterate over the words in the text and a dictionary to store the occurrences.
-    Implement error handling to handle missing files or other input issues.
+    Implement error handling to handle missing files or other input issues.'''
 
 
-5. Inventory Management System:
+
+
+
+
+
+'''5. Inventory Management System:
 
      Create a function that defines an item with a code, name, quantity, and price.
      Create a database or dictionary to store the items in inventory.

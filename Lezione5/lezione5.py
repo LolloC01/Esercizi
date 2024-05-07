90fd51cfff2ad4250e2902c8f40d92b651ba53a1
LolloC01 <colittolorenzo@gmail.com>
2024-05-07 12:24:02 +0200

    2

--- Lezione5/lezione5.py
+++ Lezione5/lezione5.py
@@ -9,9 +9,31 @@ This function should return an updated dictionary.
 
 Example: add_like(dictionary, “Road Trip”, liked=True)'''
 
+def create_playlist(name: str, titles: list):
+    album = {name: titles}
+    return album
+def add_like(album: dict, title: str, like: bool):
+    liked = {}
+    for x in album:
+        print(x)
+        if album[x][title]:
+            liked[title] = like
+    return liked
+
+
+#album0 = create_playlist("RoadTrip", {"Song 1", "Song 2", "Song 3"})
+#album1 = create_playlist("Road", {"Song 1", "Song 2", "Song "})
+#album2 = create_playlist("Trip", {"Song 1"})
+#album3 = create_playlist("Roip", {"Song 1", "Song 2", "Song 3", "song 4"})
+
+
+
+
+
 '''2. Book Collection:
 
-Write a function called add_book() that accepts an author's name and a variable number of book titles authored by them. This function should return a dictionary where the author's name is the key and the value is a list of their books. Demonstrate this function by adding books for different authors.
+Write a function called add_book() that accepts an author's name and a variable number of book titles authored by them. 
+This function should return a dictionary where the author's name is the key and the value is a list of their books. Demonstrate this function by adding books for different authors.
 
 Example: add_book("Mark Twain", ["The Adventures of Tom Sawyer", "Life on the Mississippi"])
 
@@ -19,15 +41,46 @@ Write a function called delete_book() that accepts a dictionary and the name of 
 
 Example: delete_book(dictionary, “Mark Twain”)'''
 
+def add_book(author: str, titles: list) -> dict:
+    books: dict = {}
+    books["author"] = author
+    books["books"] = titles
+    return books
+
+
+author: dict = add_book("Mark Twain", ["The Adventures of Tom Sawyer", "Life on the Mississippi"])
+author2: dict = add_book("J. k. Rowling", ["Harry Potter e la pietra filosofale", "Harry potter e il principe mezzosangue"])
+library: dict = {"author1" : author,
+                 "author2" : author2}
+print(library)
+
+def del_book(library: dict, del_author: str):
+    for x, y in library.items():
+        if y["author"] == del_author:
+            del library[x]
+            break
+
+del_book(library, "Mark Twain")
+
+print(library)
+
 '''3. Personal Info:
 
-Write a build_profile() function that accepts the name , surname,  occupation, location, and age  of a person. Make occupation, location, and age optional parameters. Use this function to create profiles for different people, demonstrating how it handles these optional parameters.
+Write a build_profile() function that accepts the name , surname,  occupation, location, and age  of a person. 
+Make occupation, location, and age optional parameters. Use this function to create profiles for different people, demonstrating how it handles these optional parameters.
 
 Example: build_profile("John", "Doe", occupation="Developer", location="USA", age=30)'''
 
+
+
+
+
+
+
 '''4. Event Organizer:
 
-Write a function called plan_event() that accepts an event name, a list of participants, and an hour. The function should return a dictionary that includes the event name and a list of the participants. Call this function with varying numbers of participants to plan different events.
+Write a function called plan_event() that accepts an event name, a list of participants, and an hour. 
+The function should return a dictionary that includes the event name and a list of the participants. Call this function with varying numbers of participants to plan different events.
 
 Example: plan_event("Code Workshop", ["Alice", "Bob", "Charlie"],”4pm”)
 

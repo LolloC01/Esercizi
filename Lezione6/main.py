5e81e66d0f422e6340956f3145a8867276fd40f5
LolloC01 <colittolorenzo@gmail.com>
2024-05-09 11:29:38 +0200

    p

--- Lezione6/main.py
+++ Lezione6/main.py
@@ -1,7 +1,7 @@
 import lezione6 as p
 
 
-Lorenzo = p.Person("Lorenzo", "Colitto", "01/12/2001", "Nuova guinea", "M")
+Lorenzo = p.Person("Lorenzo", "Colitto", "01/12/2001", "Roma", "M")
 Leonardo = p.Person("Leonardo", "Brussani", "24/07/2004", "Roma", "M")
 Luca = p.Person("luca", "re", "17/11/2002", "Roma", "M")
 print(Lorenzo.get_snn())

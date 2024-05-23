82fa931668b1776e7ba5ac03057c9cc1b2407afc
LolloC01 <colittolorenzo@gmail.com>
2024-05-23 13:50:36 +0200

    dwa

--- Lezione6.1/dice.py
+++ Lezione6.1/dice.py
@@ -35,3 +35,9 @@ class Lottery:
 #9-15. Lottery Analysis: You can use a loop to see how hard it might be to win the kind of lottery you just modeled. 
 #Make a list or tuple called my_ticket. Write a loop that keeps pulling numbers until your ticket wins. 
 #Print a message reporting how many times the loop had to run to give you a winning ticket.
+    
+    def lottery_analisys() -> None:
+        win = []
+        counter = 0
+        while len(win) != 4:
+            
